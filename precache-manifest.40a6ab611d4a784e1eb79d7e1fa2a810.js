43cdad468a047c10dd096804ab4207d1f7fcd918
Gina <fogasgeorgina@gmail.com>
2019-10-07 00:42:29 +0300

    Updates

---
+++ precache-manifest.40a6ab611d4a784e1eb79d7e1fa2a810.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fb8944de4797ace71a7c646ba61202",
+    "url": "/React-ScoreTracker/index.html"
+  },
+  {
+    "revision": "eee5a4d7e3b366309d4f",
+    "url": "/React-ScoreTracker/static/css/2.5761d638.chunk.css"
+  },
+  {
+    "revision": "06745d6ddd08eabe0390",
+    "url": "/React-ScoreTracker/static/css/main.40160a3b.chunk.css"
+  },
+  {
+    "revision": "eee5a4d7e3b366309d4f",
+    "url": "/React-ScoreTracker/static/js/2.9db165b2.chunk.js"
+  },
+  {
+    "revision": "06745d6ddd08eabe0390",
+    "url": "/React-ScoreTracker/static/js/main.b74fa607.chunk.js"
+  },
+  {
+    "revision": "400b9a6b643ecf2f5790",
+    "url": "/React-ScoreTracker/static/js/runtime-main.17502129.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-ScoreTracker/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-ScoreTracker/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-ScoreTracker/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-ScoreTracker/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-ScoreTracker/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
